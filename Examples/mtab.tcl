882e4188e00b570c05897737f10bff78fb75d9f7
Martin Weitzel <mw@tbfe.de>
2018-02-21 12:04:32 +0200

    Snapshot 2018-02-21 12:04

--- Examples/mtab.tcl
+++ Examples/mtab.tcl
@@ -75,7 +75,7 @@ proc mtab_args_to_array {arrname {rows 10} {cols 10}} {
 
 # using "named" arguments with (some)
 #
-proc mtab {args} {
+proc mtab_named_args {args} {
     # set the default values
     array set argval {-rows 10 -cols 10}
     # overwrite with what is specified
@@ -103,5 +103,67 @@ proc mtab {args} {
 }
 
 # to be called like this:
-puts [mtab -rows 5 -cols 10]
-mtab -rows 3 -to_var x; puts $x
+# puts [mtab_named_args -rows 5 -cols 10]
+# mtab_named_args -rows 3 -to_var x; puts $x
+
+# finally making everything a bit more robust
+#
+proc mtab {args} {
+    # showing help message on request
+    if {[string equal $args -help]} {
+         puts "Usage: mtab -argname argval ..."
+         puts " where -argname can be:"
+         puts "       -rows <number of rows> (default 10)"
+         puts "       -cols <number of columns> (default 10)"
+         puts "       -to_var <variable name> (optional)"
+         return
+    }
+    # check for misspelled arguments
+    foreach {named_arg val_ignored} $args {
+        switch -exact -- $named_arg {
+            -rows -
+            -cols -
+            -to_var {}
+            default {
+                puts "mtab: named argument `$named_arg` unknown (ignored)"
+            }
+        }
+    }
+
+    # set the default values
+    array set argval {-rows 10 -cols 10}
+    # overwrite with what is specified
+    array set argval $args
+
+    # check if all required arguments have values
+    foreach req_arg { -rows -cols } {
+       if {![info exists argval($req_arg)]} {
+           error "setup_project: named argument `$req_arg` missing"
+       }
+    }
+
+    # setup output to variable, if requested
+    if {[info exists argval(-to_var)]} {
+        upvar $argval(-to_var) result
+    }
+
+    # create the table in result
+    set w [string length [expr {$argval(-rows) * $argval(-cols)}]]
+    for {set r 1} {$r <= $argval(-rows)} {incr r} {
+        for {set c 1} {$c <= $argval(-cols)} {incr c} {
+	    append result [format "%*d " $w [expr {$r*$c}]]
+	}
+	append result \n
+    }
+    if {[info exists argval(-to_var)]} {
+        return "" ;# caller catches result in variable or ...
+    } else {
+        return $result ;# ... receives result via return value
+    }
+}
+
+# to be called like this:
+# mtab -help
+puts [mtab -cols 5 -rwos 4]
+mtab -to_var x; puts $x
+
